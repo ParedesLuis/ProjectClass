a09efd337d6fa3bf0f5788a853bc98f8b19fe988
LuisParedes <luisparedez@gmail.com>
2019-04-04 08:56:27 -0400

    Proyecto para programacion 2.

---
+++ Barajeo/Baraja.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barajeo
+{
+    class Baraja{
+
+        private List<int> lista;
+        public Baraja(List<int> numbers)
+        {
+            lista = numbers;
+        }
+
+        public void imprimir()
+        {
+           foreach(int lis in lista)
+            {
+                Console.WriteLine(lis);
+                
+            }
+        }
+
+        public List<int> Mezcla()
+        { 
+            List<int> List1 = new List<int>();
+            List<int> List2 = new List<int>();
+            int mitad = (lista.Count / 2);
+            int tam = lista.Count;
+            int j = 0, k = 0;
+           
+            for (int i = 0; i < lista.Count; i++)
+            {
+                if(i < lista.Count / 2)
+                {
+                    List1.Add(lista.ElementAt(i));
+                }
+                else
+                {
+                    List2.Add(lista.ElementAt(mitad));
+                    mitad++;
+                }
+                
+            }
+
+            lista.Clear();
+
+            for (int i = 0; i < tam; i ++ )
+            {
+                if (i % 2 == 0)
+                {
+                    lista.Add(List1.ElementAt(j));
+                    j++;
+                }
+                else
+                {
+                    lista.Add(List2.ElementAt(k));
+                    k++;
+                }
+                
+            }
+
+            return lista;
+
+        }
+    }
+
+   
+}
---
+++ Barajeo/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barajeo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var numbers = new List<int>( );
+            int Acount;
+
+            Console.WriteLine("Numero a Barajear: ");
+            Acount = int.Parse(Console.ReadLine());
+            Console.Clear();
+            while (true)
+            {
+                Console.WriteLine("Ingrese un numero: ");
+                numbers.Add(int.Parse(Console.ReadLine()));
+                Console.Clear();
+                Acount--;
+                if (Acount == 0)
+                {
+                    break;
+                }
+                
+            }
+            
+            var Bara = new Baraja(numbers);
+            numbers = Bara.Mezcla();
+
+            Console.WriteLine("Tamaño del vector: "+numbers.Count);
+            Console.WriteLine("Salida");
+            foreach (int lis in numbers)
+            {
+                Console.WriteLine(lis);
+
+            }
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
